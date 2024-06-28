924f77750566f72bb3de5cb0cae3cf369defc652
Philip Okugbe <16838612+Philipinho@users.noreply.github.com>
2024-06-29 00:07:49 +0100

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -59,7 +59,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: "",
+    //image: "",
     navbar: {
       title: "Docmost",
       logo: {
